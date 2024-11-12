1b037d10981f5223a403251a3002c3f602637ffe
vkraussl <thekrauss31@gmail.com>
2024-11-12 13:50:24 +0100

    implemenattion de l'authentification login register middlwares et routes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,1 @@
-const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root', // utilisateur MySQL
-  password: '',//mde passe MySQL
-  database: 'db',
-});
-
-db.connect((err) => {
-  if (err) throw err;
-  console.log('Connecté à la base de données MySQL');
-});
-
-module.exports = db;
